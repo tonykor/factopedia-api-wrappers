88df7d5e1f99c44a25a86609eb7d7a78b284ad3b
Tom Danilov <66553758+tomdanilov@users.noreply.github.com>
2021-03-18 18:22:30 +0200

    [Repository] Python wrapper for Factopedia.org
    Build 1.2

---
+++ python/factopedia/base.py
@@ -0,0 +1,188 @@
+"""
+Factopedia API Python Wrapper base.py.
+The base of the Factopedia Python library, all the features and the Factopedia class is here.
+"""
+
+from hashlib import md5
+import requests
+
+API_BASE_URL = 'api.factopedia.org'
+
+
+class Factopedia:
+
+    def __init__(self, token: str) -> None:
+        self.token = token
+        self.headers = {
+            'Accept': 'application/json; q=1.0, */*; q=0.1'
+        }
+
+        self.valid_property_settings = ['category_id', 'unit_id', 'value', 'order_by']
+        self.valid_expand_values = ['properties', 'suggestedProperties', 'parents', 'children', 'countChildren']
+        self.update_values = {
+            'parent_id': 'parents[][Objects][id]'
+        }
+
+    def get_object_by_id(self, object_id: int, api_format: bool = False, expand: list = []) -> requests.Response:
+        """
+        Request an object by his id from Factopedia.
+        Input: object_id: int, api_format: bool = False, expand: list = [].
+        Output: requests.Response.
+        """
+
+        api_format = 'format=api' if api_format else ''
+
+        for expand_value in expand:
+            if expand_value not in self.valid_expand_values:
+                raise KeyError(f'Invalid expand value "{expand_value}"')
+
+        url_params = [] if not expand else [f'expand={",".join(expand)}']
+        if api_format:
+            url_params.append(api_format)
+
+        url_params = '&'.join(url_params)
+        url = self.get_url(f'objects/{object_id}?{url_params}')
+        with requests.get(url, headers=self.headers) as response:
+            return response
+
+    def get_object(self, name: str = None, expand: list = [], parent_id: int = None, language: str = None, byproperty: str = None) -> requests.Response:
+        """
+        Request an object(s) by parameters from Factopedia.
+        Input: name: str = None, expand: list = [], parent_id: int = None, language: str = None, byproperty: str = None.
+        Output: requests.Response.
+        """
+
+        if language and len(language) != 2:
+            raise SyntaxError('Language must be 2 characters.')
+
+        for expand_value in expand:
+            if expand_value not in self.valid_expand_values:
+                raise KeyError(f'Invalid expand value "{expand_value}"')
+
+        url_params = [] if not expand else [f'expand={",".join(expand)}']
+        get_by = [('name', name), ('parent_id', parent_id), ('lang', language), ('property', byproperty)]
+        for key, value in get_by:
+            if value:
+                url_params.append(f'{key}={value}')
+
+        url_params = '&'.join(url_params)
+        url = self.get_url('objects') + '?' + url_params
+        with requests.get(url, headers=self.headers) as response:
+            return response
+
+    def create_object(self, name: str, language: str, description: str = None, main_image: str = None, parent_id: int = None, children: list = [], aliases: list = [], images: list = [], properties: list = [], links: list = []) -> requests.Response:
+        """
+        Creates an object on Factopedia.
+        Input: name: str, language: str, description: str = None, main_image: str = None, parent_id: int = None, children: list = [], aliases: list = [], images: list = [], properties: list = [], links: list = [].
+        Output: requests.Response.
+        """
+
+        if language and len(language) != 2:
+            raise SyntaxError('Language must be 2 characters.')
+
+        payload = {
+            'name': name,
+            'lang': language,
+            'description': description,
+            'parents[][Objects][id]': str(parent_id)
+        }
+
+        for index in range(len(children)):
+            payload[f'children[{index}][Objects][id]'] = str(children[index])
+
+        for index in range(len(aliases)):
+            payload[f'aliases[{index}]'] = aliases[index]
+
+        for link in links:
+            payload[f'Links[{links["property"]}][url]'] = links['url']
+
+        files = []
+        for i in range(len(images)):
+            image_type = 'image/png' if '.png' in images[i] else 'image/jpeg'
+            files.append((f'Objects[imageFiles][{i}]', (images[i], open(images[i], 'rb'), image_type)))
+
+        if main_image:
+            main_image_checksum = [md5(open(x, 'rb').read()).hexdigest() for x in images if x == main_image]
+            if main_image_checksum:
+                payload['main_image'] = main_image_checksum[0]
+            else:
+                raise NameError('The main_image file you provided isn\'t exist.')
+
+        for _property in properties:
+            payload[f'objectsPropertiesValues[{_property["id"]}][ObjectsPropertiesValues][property_id]'] = _property['id']
+            payload[f'objectsPropertiesValues[{_property["id"]}][ObjectsPropertiesValues][type]'] = _property['type']
+
+            for key in _property.keys():
+                if key in ['id', 'type']:
+                    continue
+
+                if key not in self.valid_property_settings:
+                    raise KeyError(f'The key "{key}" of the property is invalid.')
+
+                payload[f'objectsPropertiesValues[{_property["id"]}][ObjectsPropertiesValues][{key}]'] = _property[key]
+
+        url = self.get_url('objects')
+        with requests.post(url, headers=self.headers, data=payload, files=files) as response:
+            return response
+
+    def update_object(self, object_id: int, aliases: list = [], **kwargs) -> requests.Response:
+        """
+        Updates an object by his id. Updated fields are the kwargs.
+        Input: object_id: int, aliases: list = [], **kwargs.
+        Output: requests.Response.
+        """
+
+        if not kwargs:
+            raise ValueError(f'You didn\'t provide any changes to object id {object_id}.')
+
+        object_payload = self.get_object_by_id(object_id).json()
+        object_payload['aliases'] += aliases
+
+        for key in kwargs.keys():
+            if key in self.update_values:
+                key = self.update_values[key]
+
+            object_payload[key] = kwargs[key]
+
+        url = self.get_url(f'objects/{object_id}')
+        with requests.put(url, headers=self.headers, data=object_payload) as response:
+            return response
+
+    def get_property(self, name: str, language: str) -> requests.Response:
+        """
+        Returns a property by name and lagnuage.
+        Input: name: str, language: str.
+        Output: requests.Response.
+        """
+
+        if language and len(language) != 2:
+            raise SyntaxError('Language must be 2 characters.')
+
+        url_params = f'filter[name]={name}&lang={language}'
+        url = self.get_url('properties') + '?' + url_params
+
+        with requests.get(url, headers=self.headers) as response:
+            return response
+
+    def create_property(self, name: str, language: str, property_type: str) -> requests.Response:
+        """
+        Creates a new property using the parameters.
+        Input: name: str, language: str, property_type: str.
+        Output: requests.Response.
+        """
+
+        if language and len(language) != 2:
+            raise SyntaxError('Language must be 2 characters.')
+
+        payload = {
+            'name': name,
+            'lang': language,
+            'type': property_type
+        }
+
+        url = self.get_url('properties')
+        with requests.post(url, headers=self.headers, data=payload) as response:
+            return response
+
+    def get_url(self, endpoint: str) -> str:
+        return f'https://{self.token}:@{API_BASE_URL}/{endpoint}'

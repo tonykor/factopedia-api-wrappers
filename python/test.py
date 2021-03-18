ef8cff78df5e92d30a153522b2b3e9520a8db676
Tom Danilov <66553758+tomdanilov@users.noreply.github.com>
2021-03-18 18:21:34 +0200

    test.py
    Change images in order to test

---
+++ python/test.py
@@ -0,0 +1,39 @@
+import factopedia
+
+def main() -> None:
+    api = factopedia.Factopedia('nuhdSQ5XdgY-lmnXGhmUndgMX6gyomUA')
+
+    # print('Get an object by id 100 expand=["parents", "propertirs"]')
+    # response = api.get_object_by_id(100, expand=['parents', 'properties'])
+    # print('Response:', response, response.json(), '\n')
+
+    # print('Get an object by filter [name="Cars"]')
+    # response = api.get_object(name='Cars', expand=['parents', 'children'])
+    # print('Response:', response, response.json(), '\n')
+
+    print('Creating a new object named "test" [lang="en", parent_id=42102, description="Hello hello", images=["map.png", "moon.jpg"], main_image="map.png", proporties=[{\'id\': 151, \'type\': \'text\', \'value\': \'Just a test\'}], aliases=["helloman", "hellowoman"]')
+    f_response = api.create_object('test', 'en', parent_id=42102, description='Hello hello', aliases=['helloman', 'hellowoman'], images=['map.png', 'moon.jpg'], main_image='map.png', properties=[{'id': 151, 'type': 'text', 'value': 'Just a test'}])
+    print('Response:', f_response, f_response.json(), '\n')
+
+    print('Get an object by filter [name="Chemistry"]')
+    response = api.get_object(name='Mobile phones', expand=['parents', 'properties'])
+    print('Response:', response, response.json(), '\n')
+
+    print('Get a property by filter [name="Technology", lang="en"]')
+    response = api.get_property('Technology', 'en')
+    print('Response:', response, response.json(), '\n')
+
+    print('Get an object by id 100')
+    response = api.get_object_by_id(100)
+    print('Response:', response, response.json(), '\n')
+
+    print(f'Update the first response page by his id [{f_response.json()["id"]}]')
+    response = api.update_object(f_response.json()['id'], name='Rhino testing')
+    print('Response:', response, response.json(), '\n')
+
+    print('Create a new property name="Technology", lang="en", type="text"')
+    response = api.create_property(name='Technology', language='en', property_type='text')
+    print('Response:', response, response.json())
+
+if __name__ == '__main__':
+    main()
